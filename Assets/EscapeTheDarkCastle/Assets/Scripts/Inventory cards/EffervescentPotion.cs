5d180c333051b78760ce8bd03753b2953ceb9c4a
ryanDMaule <davidmaule98@gmail.com>
2024-01-03 22:46:58 +0000

    Created effervescent potion prefab

---
+++ Assets/EscapeTheDarkCastle/Assets/Scripts/Inventory cards/EffervescentPotion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffervescentPotion : Card
+{
+    public override void useItem(PlayerBase player)
+    {
+        //do nothing
+    }
+}
