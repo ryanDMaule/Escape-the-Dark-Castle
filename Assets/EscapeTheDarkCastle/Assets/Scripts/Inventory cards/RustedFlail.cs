61b3c4068027af7c507ecb3d96f28bb0c4060c76
ryanDMaule <davidmaule98@gmail.com>
2024-01-03 23:03:13 +0000

    Added Rusted flail prefab

---
+++ Assets/EscapeTheDarkCastle/Assets/Scripts/Inventory cards/RustedFlail.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RustedFlail : Card
+{
+    public override void useItem(PlayerBase player)
+    {
+        //implement logic
+    }
+
+}
