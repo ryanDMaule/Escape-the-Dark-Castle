8bc633f00e1c422d9229b66d62e094cd13de1221
ryanDMaule <davidmaule98@gmail.com>
2024-02-02 20:14:07 +0000

    pause screen tweaks
    - Tapping the cog icon now toggles the open state so it will now close if pressed when the pause screen is open
    
    - Pausing will also pause the music

--- Assets/EscapeTheDarkCastle/Assets/Scripts/PauseScreen.cs
+++ Assets/EscapeTheDarkCastle/Assets/Scripts/PauseScreen.cs
@@ -7,14 +7,32 @@ public class PauseScreen : MonoBehaviour
 {
     [Header("Game objects")]
     public GameObject pauseScreen;
+
+    [Header("Events")]
+    public GameEvent PauseMusic;
+    public GameEvent ResumeMusic;
+
     public void closePauseScren()
     {
+        ResumeMusic.Raise();
         pauseScreen.SetActive(false);
     }
 
     public void openPauseScren()
     {
         pauseScreen.SetActive(true);
+        PauseMusic.Raise();
+    }
+
+    public void togglePauseScreen()
+    {
+        if (pauseScreen.gameObject.activeInHierarchy)
+        {
+            closePauseScren();
+        } else
+        {
+            openPauseScren();
+        }
     }
 
 }
